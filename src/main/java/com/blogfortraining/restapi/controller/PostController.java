0ed9e8eca84ea56e0066a67c14ff74d80f55b6c7
shadai-rafael <shadai.lopez.garcia@gmail.com>
2022-04-22 22:21:19 -0500

    Adding in memory users for spring security
    Signed-off-by: shadai-rafael <shadai.lopez.garcia@gmail.com>

--- src/main/java/com/blogfortraining/restapi/controller/PostController.java
+++ src/main/java/com/blogfortraining/restapi/controller/PostController.java
@@ -31,6 +31,7 @@ import com.blogfortraining.restapi.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,8 +57,9 @@ public class PostController {
         this.postService = postService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<PostDTO> creaPost(@Valid @RequestBody PostDTO postDTO){
+    public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO postDTO){
         System.out.println(postDTO);
         return new ResponseEntity<>(postService.createPost(postDTO),HttpStatus.CREATED);
     }
